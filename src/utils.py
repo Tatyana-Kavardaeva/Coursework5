e4b476f090f437bba2024b8462950e620e8dd2c7
Tatyana Kavardaeva <solarpoint@yandex.ru>
2024-06-24 17:02:48 +0300

    add create_database in utils.py

---
+++ src/utils.py
@@ -0,0 +1,10 @@
+
+
+def get__hh__data(url: str, employer_id: int) -> list[dict]:
+    """ Получение списка вакансий по id работодателя с помощью API hh.ru """
+    pass
+
+
+def create_database(name_db: str, params: dict) -> None:
+    """ Создание базы данных """
+    pass
