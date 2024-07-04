a8f83202725e8be1b1850964715a9372366e0a23
Tatyana Kavardaeva <solarpoint@yandex.ru>
2024-07-04 11:49:45 +0300

    update main.py add check DBManager

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from src.utils import get_employers, get_vacancies, create_database, save_data_to_vacancies, save_data_to_employers
 from src.class_dbmanager import DBManager
 from config import config
-from tqdm import tqdm
 
 
 def main():
@@ -27,7 +26,6 @@ def main():
     print()
     db_name = input("Введите название базы данных: ")
     create_database(db_name, params)
-    print(f"База данных '{db_name}' создана")
     # Сохраняем данные о работодателях
     save_data_to_employers(employers, db_name, params)
     print("Данные о работодателях успешно сохранены в БД")
@@ -35,14 +33,30 @@ def main():
     for k, v in dict_vac.items():
         save_data_to_vacancies(v, db_name, params)
     print("Данные о вакансиях успешно сохранены в БД")
+    print()
 
     # Проверяем взаимодействие с базой данных через DBManager
     db_manager = DBManager(db_name, params)
-    # print(db_manager.get_companies_and_vacancies_count())
-    # print(db_manager.get_all_vacancies())
-    # print(db_manager.get_avg_salary())
-    # print(db_manager.get_vacancies_with_higher_salary())
-    print(db_manager.get_vacancies_with_keyword('python'))
+    answer1 = input('Хотите посмотреть список всех компаний и количество вакансий у каждой компании? (да/нет): ').lower()
+    print(db_manager.get_companies_and_vacancies_count()) if answer1 == 'да' else None
+    print()
+    answer2 = input('Показать  все вакансии с указанием названия компании, '
+                    'названия вакансии, зарплаты и ссылки на вакансию? (да/нет): ').lower()
+    print(db_manager.get_all_vacancies()) if answer2 == 'да' else None
+    print()
+    answer3 = input('Рассчитать среднюю заработную плату по каждой вакансии? (да/нет): ').lower()
+    print(db_manager.get_avg_salary()) if answer3 == 'да' else None
+    print()
+    answer4 = input('Хотите посмотреть список всех вакансий, '
+                    'у которых зарплата выше средней по всем вакансиям? (да/нет): ').lower()
+    print(db_manager.get_vacancies_with_higher_salary()) if answer4 == 'да' else None
+    print()
+    word = input('Введите ключевое слово для поиска в названии вакансий: ').lower()
+    result = db_manager.get_vacancies_with_keyword(word)
+    if result is []:
+        print("Вакансии не найдены")
+    else:
+        print(db_manager.get_vacancies_with_keyword(word))
 
 
 if __name__ == "__main__":
