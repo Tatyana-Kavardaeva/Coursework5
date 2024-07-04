2be14a7695d1e6a1742bda666bffaac64447b83d
Tatyana Kavardaeva <solarpoint@yandex.ru>
2024-07-04 11:51:34 +0300

    update get_avg_salary, get_vacancies_with_higher_salary, get_vacancies_with_keyword in class_dbmanager.py

--- src/class_dbmanager.py
+++ src/class_dbmanager.py
@@ -3,7 +3,6 @@ from tabulate import tabulate
 
 
 class DBManager:
-
     dbname: str
     params: dict
 
@@ -15,7 +14,7 @@ class DBManager:
         """ Получает список всех компаний и количество вакансий у каждого работодателя """
         conn = psycopg2.connect(dbname=self.dbname, **self.params)
         with conn.cursor() as cur:
-            cur.execute("""SELECT company_name, open_vacancies FROM employers ORDER by open_vacancies DESC""")
+            cur.execute("""SELECT company_name, open_vacancies FROM employers ORDER BY open_vacancies DESC""")
             rows = cur.fetchall()
 
         conn.commit()
@@ -31,13 +30,13 @@ class DBManager:
         """
         conn = psycopg2.connect(dbname=self.dbname, **self.params)
         with conn.cursor() as cur:
-            cur.execute("""SELECT v.vacancy_name, e.company_name, v.salary_from, v.salary_to, v.currency, v.url 
+            cur.execute("""SELECT v.vacancy_name, e.company_name, v.salary_to, v.salary_to, v.currency, v.url 
                     FROM vacancies as v
                     JOIN employers as e USING(employer_id)""")
             rows = cur.fetchall()
         conn.commit()
         conn.close()
-        headers = ["Vacancy name", "Company", "Зарплата от", "Зарплата до", "Валюта", "Cсылка на вакансию"]
+        headers = ["Vacancy name", "company name", "salary to", "salary to", "currency", "url"]
         result = tabulate(rows, headers=headers)
         return result
 
@@ -45,35 +44,43 @@ class DBManager:
         """ Получает среднюю зарплату по вакансиям """
         conn = psycopg2.connect(dbname=self.dbname, **self.params)
         with conn.cursor() as cur:
-            cur.execute("""SELECT vacancy_name, AVG(salary_to + salary_from) as avg_salary 
+            cur.execute("""SELECT vacancy_name, AVG(salary_to + salary_from) as higher_salary,  currency
             FROM vacancies 
-            GROUP BY vacancy_name
-            ORDER BY avg_salary DESC""")
+            WHERE salary_to <> 0 AND salary_from <> 0 AND currency = 'RUR'
+            GROUP BY vacancy_name, currency
+            ORDER BY higher_salary DESC""")
             rows = cur.fetchall()
         conn.commit()
         conn.close()
-        headers = ["Vacancy name", "Higher_salary"]
+        headers = ["Vacancy name", "higher salary", "currency"]
         result = tabulate(rows, headers=headers)
         return result
 
     def get_vacancies_with_higher_salary(self):
-        """ Получает список всех вакансий, у которых зарплата выше средней по всем вакансиям."""
+        """ Получает список всех вакансий, у которых зарплата выше средней по всем вакансиям """
         conn = psycopg2.connect(dbname=self.dbname, **self.params)
         with conn.cursor() as cur:
-            cur.execute("""SELECT vacancy_name, salary_from, salary_to, currency FROM vacancies WHERE salary_to > (SELECT AVG(salary_from + salary_to) 
-            FROM vacancies) ORDER BY salary_to DESC""")
+            cur.execute("""SELECT vacancy_name, AVG(salary_from + salary_to) as higher_salary, currency 
+                        FROM vacancies 
+                        WHERE salary_to > (SELECT AVG(salary_from + salary_to) FROM vacancies 
+                        WHERE salary_from <> 0 AND salary_to <> 0 AND currency = 'RUR')
+                        AND salary_from <> 0 AND salary_to <> 0 AND currency = 'RUR'
+                        GROUP BY vacancy_name, currency
+                        ORDER BY higher_salary DESC""")
             rows = cur.fetchall()
         conn.commit()
         conn.close()
-        headers = ["Vacancy name", "salary_from", "salary_to", "currency"]
+        headers = ["Vacancy name", "higher salary", "currency"]
         result = tabulate(rows, headers=headers)
         return result
 
     def get_vacancies_with_keyword(self, keyword):
-        """ Получает список всех вакансий, в названии которых содержатся переданные в метод слова, например python """
+        """ Получает список всех вакансий, в названии которых содержатся переданные в метод слова,
+            например 'python' """
         conn = psycopg2.connect(dbname=self.dbname, **self.params)
         with conn.cursor() as cur:
-            cur.execute(f"""SELECT vacancy_name, city, url FROM vacancies WHERE vacancy_name LIKE ('%{keyword}%')""")
+            cur.execute(f"""SELECT vacancy_name, city, url FROM vacancies 
+            WHERE LOWER(vacancy_name) LIKE ('%{keyword.lower()}%')""")
             rows = cur.fetchall()
         conn.commit()
         conn.close()
